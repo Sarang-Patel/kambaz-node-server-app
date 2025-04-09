e1a65c386a5250bbf118ab5911035792a5c8a1c9
Sarang Ajay Patel <sarangp203@gmail.com>
2025-04-09 01:56:39 -0400

    assignments done

---
+++ Kambaz/Assignments/schema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    _id: String,
+    title: String,
+    course: { type: String, ref: "CourseModel" },
+    description: String,
+    points: Number,
+    dueDate: Date,
+    availableFrom: Date,
+    availableTo: Date,
+  },
+  { collection: "assignments" }
+);
+
+export default schema;
